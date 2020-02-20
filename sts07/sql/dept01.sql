13fc1570630c6c925818f6dc0f835a81daeeb1be
Kimej0924 <jjeong_0924@naver.com>
2020-02-20 18:02:38 +0900

    mybatis

--- sts07/sql/dept01.sql
+++ sts07/sql/dept01.sql
@@ -5,8 +5,13 @@ CREATE TABLE dept01 (
   dname varchar(45) NOT NULL,
   loc varchar(45) DEFAULT NULL,
   PRIMARY KEY (deptno)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO dept01 (`deptno`,`dname`,`loc`) VALUES (1,'영업부','서울');
-INSERT INTO dept01 (`deptno`,`dname`,`loc`) VALUES (2,'총무부','부산');
-INSERT INTO dept01 (`deptno`,`dname`,`loc`) VALUES (3,'회계부','대구');
+-- 시퀀스값 바꿀 때
+-- alter table dept01 auto_increment=1;
+
+-- dummy
+INSERT INTO dept01 (`dname`,`loc`) VALUES ('영업부','서울');
+INSERT INTO dept01 (`dname`,`loc`) VALUES ('총무부','부산');
+INSERT INTO dept01 (`dname`,`loc`) VALUES ('회계부','대구');
+commit;
--- sts07/sql/emp01.sql
+++ sts07/sql/emp01.sql
@@ -9,9 +9,14 @@ CREATE TABLE emp01 (
   PRIMARY KEY (sabun),
   KEY emp01_fk_idx (deptno),
   CONSTRAINT emp01_fk FOREIGN KEY (deptno) REFERENCES dept01 (deptno) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ½ÃÄö½º°ª ¹Ù²Ü ¶§
+-- alter table emp01 auto_increment=1;
+
 -- dummy
-INSERT INTO emp01 (`sabun`,`name`,`nalja`,`pay`,`deptno`) VALUES (1,'test1','2020-02-17 11:59:40',1000,1);
-INSERT INTO emp01 (`sabun`,`name`,`nalja`,`pay`,`deptno`) VALUES (2,'test2','2020-02-17 11:59:41',2000,3);
-INSERT INTO emp01 (`sabun`,`name`,`nalja`,`pay`,`deptno`) VALUES (3,'test3','2020-02-17 11:59:42',3000,1);
-INSERT INTO emp01 (`sabun`,`name`,`nalja`,`pay`,`deptno`) VALUES (4,'test4','2020-02-17 11:59:43',4000,2);
+INSERT INTO emp01 (`name`,`nalja`,`pay`,`deptno`) VALUES ('test1','2020-02-17 11:59:40',1000,1);
+INSERT INTO emp01 (`name`,`nalja`,`pay`,`deptno`) VALUES ('test2','2020-02-17 11:59:41',2000,3);
+INSERT INTO emp01 (`name`,`nalja`,`pay`,`deptno`) VALUES ('test3','2020-02-17 11:59:42',3000,1);
+INSERT INTO emp01 (`name`,`nalja`,`pay`,`deptno`) VALUES ('test4','2020-02-17 11:59:43',4000,2);
+commit;
