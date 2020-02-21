78eb286b24894dc05d9242daa0eb3b499553c739
Kimej0924 <jjeong_0924@naver.com>
2020-02-21 11:38:51 +0900

    mybatis

---
+++ sts08/src/main/java/com/bit/project03/controller/EmpController.java
@@ -0,0 +1,34 @@
+package com.bit.project03.controller;
+
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bit.project03.model.EmpDao;
+import com.bit.project03.model.entity.EmpVo;
+
+@Controller
+public class EmpController {
+	@Autowired
+	EmpDao empDao;
+
+	@RequestMapping("/emp/list")
+	public String list(Model model) throws SQLException {
+		model.addAttribute("list", empDao.selectAll());
+		return "empList";
+	}
+	
+	@RequestMapping(value = "emp/add", method = RequestMethod.GET)
+	public String add() {return "empAdd";}
+	
+	@RequestMapping(value = "emp/add", method = RequestMethod.POST)
+	public String add(@ModelAttribute EmpVo bean) throws SQLException {
+		empDao.insertOne(bean);
+		return "redirect:list";
+	}
+}
---
+++ sts08/src/main/java/com/bit/project03/model/EmpDaoImpl.java
@@ -0,0 +1,50 @@
+package com.bit.project03.model;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bit.project03.model.entity.EmpVo;
+
+public class EmpDaoImpl implements EmpDao {
+	@Autowired
+	SqlSession sqlSession;
+
+	@Override
+	public List<EmpVo> selectAll() throws SQLException {
+		return sqlSession.selectList("emp.selectAll");
+	}
+
+	@Override
+	public EmpVo selectOne(int key) throws SQLException {
+		return null;
+	}
+
+	@Override
+	public void insertOne(EmpVo bean) throws SQLException {
+		sqlSession.insert("emp.insertOne", bean);
+	}
+
+	@Override
+	public int updateOne(EmpVo bean) throws SQLException {
+		return 0;
+	}
+
+	@Override
+	public int deleteOne(int key) throws SQLException {
+		return 0;
+	}
+
+	@Override
+	public int updatePay(int pay) throws SQLException {
+		return 0;
+	}
+
+	@Override
+	public int selectTotal() throws SQLException {
+		return 0;
+	}
+
+}
